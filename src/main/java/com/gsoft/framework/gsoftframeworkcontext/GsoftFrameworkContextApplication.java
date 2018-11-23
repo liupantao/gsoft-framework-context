f58176d2b0f6125a414422be5dc97824f9b1a385
liupantao <804878782@qq.com>
2018-11-23 17:39:24 +0800

    Initial commit

---
+++ src/main/java/com/gsoft/framework/gsoftframeworkcontext/GsoftFrameworkContextApplication.java
@@ -0,0 +1,12 @@
+package com.gsoft.framework.gsoftframeworkcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsoftFrameworkContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsoftFrameworkContextApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gsoft/framework/gsoftframeworkcontext/GsoftFrameworkContextApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gsoft.framework.gsoftframeworkcontext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GsoftFrameworkContextApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
